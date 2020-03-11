a52aee6e27a0ce9c6c8f051bc2c0b3b2bd2c34aa
Kaustuv942 <kaustuv942@gmail.com>
2020-03-11 20:15:15 +0530

    Error Handling

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
+
 
 
 app = Flask(__name__)
@@ -13,4 +16,12 @@ migrate = Migrate(app, db)
 login= LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not app.debug:
+	if app.config['MAIL_SERVER']:
+		auth = None
+		if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+			auth = (app.config['MAIL_USUERNAME'], app.config['MAIL_PASSWORD'])
+		secure = None
--- config.py
+++ config.py
@@ -10,3 +10,12 @@ class Config(object):
 		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	
+
+	#sends a mail whenever there is an error
+
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['kaustuv942@gmail.com']
